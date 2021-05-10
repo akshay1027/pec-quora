10124a3fda642d9e510d2164d4e01cd8bfff56a5
Akshay R R <65683151+akshay1027@users.noreply.github.com>
2021-05-10 21:45:38 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+import QuestionBox from "./components/QuestionBox";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import NavBar from "./components/Navbar"
+import QuesstionList from "./components/QuestionList";
+import axios from "axios"
+import QuestionScreen from "./components/QuestionScreen";
+import Background from "./components/Background";
+import About from "./components/about";
+
+// use callbacks instead of "function App()"
+
+
+const App = () =>  {
+  
+  // prop drilling
+
+  const [authStatus, setAuthStatus] = useState(false);
+  const [profileImage, setProfileImage] = useState(null);
+  const [username, setUsername] = useState(null);
+
+  // whenever anything happens in the "App", useEffect is triggered.
+
+  useEffect(()=>{
+    axios
+        .get("/isLoggedIn", {withCredentials: true})
+        .then((response)=>{
+            setAuthStatus(response.data.authStatus);
+            setProfileImage(response.data.profileImage);
+            setUsername(response.data.username);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+  });
+
+  return (
+    <Router className="App">
+      <NavBar profileImage={profileImage}/> {/* header will be present in all pages */}
+         <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route path="/questions/:id" render={(props) => (  
+            <QuestionScreen authStatus={authStatus} profileImage={profileImage} username={username}
+                {...props} />
+            )} exact />
+
+          <Route path="/quora">
+          <div style={{background:"rgba(25, 28, 31)"}}>
+            <Background />
+            <QuestionBox authStatus={authStatus} profileImage={profileImage} username={username}/>
+            <QuesstionList />
+            </div>
+          </Route>
+        
+          <Route path="/">
+          <About />
+          </Route>
+
+          </Switch>
+    </Router>
+  );
+} 
+
+export default App;
+
---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+  const Header = () => {
+    
+    // to get window location!
+    const location = window.location.href;
+
+    return(
+        <div style={{padding:"2rem"}}>
+
+        </div>
+            
+    )
+}; 
+
+export default Header;
